9b9fe6456749d580df8af6b80ee22a1921ccb5e9
Nikitadevil717 <warlok88@mail.ru>
2019-11-01 21:28:02 +0300

    test

---
+++ app/js/index.js
@@ -0,0 +1,21 @@
+"use strict";
+
+$('.slider__container').slick({
+  slidesPerRow: 3,
+  rows: 2,
+  dots: true,
+  responsive: [{
+    breakpoint: 1201,
+    settings: {
+      slidesPerRow: 2
+    }
+  }, {
+    breakpoint: 768,
+    settings: {
+      slidesPerRow: 1,
+      rows: 1,
+      arrow: false,
+      infinite: false
+    }
+  }]
+});
